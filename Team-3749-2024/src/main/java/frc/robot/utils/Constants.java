617bdbc1efdf9f072ee46f9e0df50a95b7b6db95
Don Tran <donqt15@gmail.com>
2024-01-11 09:30:31 -0800

    added basic code for setpoints if we end up using

--- Team-3749-2024/src/main/java/frc/robot/utils/Constants.java
+++ Team-3749-2024/src/main/java/frc/robot/utils/Constants.java
@@ -28,6 +28,21 @@ public class Constants {
         public static final double elevatorMinHeight = 0; // TODO: change to correct height
     }
 
+    public static enum Setpoints { // How to make dynamic setpoint?
+        // Setpoint(ElevatorExtensiom, ShooterAngle)
+        STOW(0, 0),
+        AMP(1, 45),
+        HANG(1, 90);
+
+        public double elevatorExtension;
+        public double shooterAngle;
+
+        Setpoints(double elevatorExtension, double shooterAngle) {
+            this.elevatorExtension = elevatorExtension;
+            this.shooterAngle = shooterAngle;
+        }
+    }
+
     public static final class ControllerConstants {
         public static final double deadband = 0.1;
     }
