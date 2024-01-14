12951894750dc3ce0cf7c03df2cf10f5f582c159
ple1ades <jadonjkl@gmail.com>
2024-01-13 16:31:23 -0800

    Test of different way to get pose

--- Team-3749-2024/src/main/java/frc/robot/commands/vision/PhotonSim.java
+++ Team-3749-2024/src/main/java/frc/robot/commands/vision/PhotonSim.java
@@ -3,6 +3,7 @@ package frc.robot.commands.vision;
 
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
@@ -18,7 +19,7 @@ public class PhotonSim extends Command {
     private Swerve swerve;
 
     // Pose2d to store the estimated global pose
-    Pose2d estimatedPose2d;
+    Transform3d estimatedPose2d;
 
     // Constructor for PhotonSim command
     public PhotonSim(){
@@ -34,15 +35,21 @@ public class PhotonSim extends Command {
     @Override
     public void execute(){
         // Checking if Limelight has a target in the latest result
-        if (limelight.getEstimatedGlobalPose(swerve.getPose()).isPresent()){
+        if (limelight.getLatestResult().hasTargets()){
             // Logging information to SmartDashboard
             SmartDashboard.putNumber("AHHH", 0);
 
             // Getting the estimated global pose using Limelight and SwerveDrive pose
-            estimatedPose2d = limelight.getEstimatedGlobalPose(swerve.getPose()).get().estimatedPose.toPose2d();
+            try{
+                // estimatedPose2d = limelight.getEstimatedGlobalPose(swerve.getPose()).get().estimatedPose.toPose2d();
+                estimatedPose2d = limelight.getBestTarget(limelight.getLatestResult()).getBestCameraToTarget();
+                // Logging Limelight odometry information to SmartDashboard
+                SmartDashboard.putNumberArray("Limelight Odometry", new double[] { 0.35104432302 * estimatedPose2d.getX(), 0.35104432302 * estimatedPose2d.getY(), estimatedPose2d.getRotation().getY() });
+                
+            }
+            catch (Exception e){
 
-            // Logging Limelight odometry information to SmartDashboard
-            SmartDashboard.putNumberArray("Limelight Odometry", new double[] { estimatedPose2d.getX(), estimatedPose2d.getY(), estimatedPose2d.getRotation().getDegrees() });
-        }
+            }
+         }
     }
 }
